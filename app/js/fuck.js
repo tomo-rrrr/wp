c0177d25e87c4125b241897f58703ce4fea1bcb1
tomo-rrrr <761163902@qq.com>
2017-03-07 22:49:43 +0800

    bundle has been deleted

--- app/js/fuck.js
+++ app/js/fuck.js
@@ -1,5 +1,7 @@
 module.exports = (function (){
+	var h1 = document.createElement("h1");
+		h1.innerHTML = "what happen to da bundlejs???";
     return {
-    	fuck: "fark that u little motherfucker!!?!"
+    	fuck: h1
     }
 })();
--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,2 @@
-var greeter = require("./greet");
 var fuck    = require("./fuck");
-document.body.appendChild(greeter.h1);
-document.querySelector("h1").innerText = (fuck.fuck);
+document.body.appendChild(fuck.fuck);
